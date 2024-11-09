58df1bf7b7b2c01e001a6c64df6228c2158f3ed7
Clearsu <clearwater1219@gmail.com>
2024-11-09 05:01:15 +0100

    feat: pages

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import NavCircle from "../Components/NavCircle";
+import PageWrapper from "../Components/PageWrapper";
+
+const Home = () => {
+  return (
+    <PageWrapper>
+      <NavCircle />
+    </PageWrapper>
+  );
+};
+
+export default Home;
